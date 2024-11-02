52f8d27c720a6ad4f007717b3ea7f6cf42a1ea27
Sam Gilman <samsgilman@gmail.com>
2024-11-01 22:42:46 -0400

    prisma

--- app/api/ask/route.ts
+++ app/api/ask/route.ts
@@ -1,5 +1,8 @@
 import OpenAI from 'openai';
 import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 const client = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'],
@@ -10,6 +13,12 @@ export async function POST(request: Request) {
 
   console.log("process.env['OPENAI_API_KEY']:", process.env['OPENAI_API_KEY']);
 
+  const partner = await prisma.partner.findUnique({
+    where: {
+      id: 1
+    }
+  })
+
   try {
     const chatCompletion = await client.chat.completions.create({
       messages: [
@@ -18,7 +27,7 @@ export async function POST(request: Request) {
       model: 'gpt-3.5-turbo',
     });
 
-    return NextResponse.json({ answer: chatCompletion.choices[0].message.content });
+    return NextResponse.json({ answer: partner?.name ?? '' + chatCompletion.choices[0].message.content });
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
---
+++ prisma/migrations/20241102020709_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Partner" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+
+    CONSTRAINT "Partner_pkey" PRIMARY KEY ("id")
+);
